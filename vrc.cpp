9ffbfbdeb9cfa5793193f1d0bafc2c3d388c65f8
renatoseb <renatorodriguezllanos@gmail.com>
2021-12-02 15:37:39 -0500

    done

--- vrc.cpp
+++ vrc.cpp
@@ -1,50 +1,22 @@
-#include<stdio.h>
 #include<bits/stdc++.h>
-int binary(char);
-void parity(int[]);
-
 
 int arr[9],arr1[9];
 
 using namespace std;
 
-int main()
-{       
-	char chr1, chr;
-	cout << "Ingresar datos: ";
-	cin >> chr >> chr1;
-
-	binary(chr);
-	cout << endl << "Binary: ";
-
-	for(int i=0 ; i<8 ; i++)
-	{
-		arr1[i]=arr[i];
-		cout << arr[i] << " ";
-	}
-
-	parity(arr);
-	binary(chr1);
-	cout << endl << endl << "Binary : ";
-	for(int i = 0 ; i < 8 ; i++)
-	{
-		cout << arr[i] << " ";
-	}
-	parity(arr);
-}
-
-
-
-void parity(int a[])
+void settingParity(int a[])
 {
 	int count = 0;
+    // counting the number of ones
 	for(int i = 0; i < 8; i++)
 		if(a[i]) count++;
-
+    
+    // checking the parity
 	if(count%2==0) a[8]=0;
 	else a[8]=1;
-	
-	count=0;
+    
+    count = 0;
+    // printing the byte
 	cout << endl << "VRC: " << endl; 
 	for(int i = 0; i < 9; i++)
 	{
@@ -53,13 +25,12 @@ void parity(int a[])
 	}
 }
 
-int binary(char letter)
+void toBinary(char letter)
 {
-	int rem;
 	int j = 0;
-
 	int bin = stoi(bitset<8>(letter).to_string());
-
+    
+    // insert letter in binary form in arr 
 	for(int i = 7; i >= 0; --i)
 	{
 		arr[i] = bin % 10;
@@ -67,11 +38,45 @@ int binary(char letter)
 		if(arr[i] == 1) j++;  
 	}
 
-
-	if(j%2==0)
-		arr[8] = 0;
-	else
-		arr[8] = 1;
-
-	return(0);
+    if(j%2 == 0)
+        arr[8] = 0;
+    else 
+        arr[8] = 1;
 }
+
+
+int main()
+{       
+	char chr1, chr;
+	cout << "Data to send: ";
+	cin >> chr;
+    
+    // Converting data to send in binary
+	toBinary(chr);
+	cout << endl << "Binary of the data sent: ";
+	for(int i=0 ; i<8 ; i++)
+	{
+		arr1[i]=arr[i];
+		cout << arr[i] << " ";
+	}
+    
+    // Checking de parity of the data sent
+	settingParity(arr);
+
+	cout << "\nData received: ";
+    cin >> chr1;
+    
+    // Converting the received data to binary
+    toBinary(chr1);
+	cout << endl << endl << "Binary of received data: ";
+	for(int i = 0 ; i < 8 ; i++)
+		cout << arr[i] << " ";
+
+    // Checking the parity of the received data
+	settingParity(arr);
+    
+}
+
+
+
+
